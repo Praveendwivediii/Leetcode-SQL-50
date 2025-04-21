3175b2e55cae943218f1d300239ccc226ca65076
Praveen <praveendwivedi1245@gmail.com>
2025-04-21 23:17:30 +0530

    Added SQL File

--- 1581. Customer Who Visited but Did Not Make Any Transactions/1581. Customer Who Visited but Did Not Make Any Transactions.sql
+++ 1581. Customer Who Visited but Did Not Make Any Transactions/1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -1,2 +1,6 @@
-SELECT
-FROM
+SELECT v.customer_id , count(v.visit_id) AS count_no_trans
+FROM visits AS v
+LEFT JOIN transactions AS t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
