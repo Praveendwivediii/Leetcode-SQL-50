c617c51191b1083c8bc489a37349ade8d221db3d
Praveen <praveendwivedi1245@gmail.com>
2025-04-27 22:31:07 +0530

    Added SQL File

--- 197. Rising Temperature/197. Rising Temperature.sql
+++ 197. Rising Temperature/197. Rising Temperature.sql
@@ -1,1 +1,16 @@
-SELECT
+# First Method
+  
+SELECT W1.id
+FROM Weather AS W1
+JOIN Weather AS W2
+ON DATEDIFF(W1.recordDate, W2.recordDate) = 1
+AND W1.temperature > W2.temperature;
+_ _ _
+
+# Second Method
+
+SELECT W1.id
+FROM Weather AS W1
+JOIN Weather AS W2
+ON SUBDATE(W1.recordDate, 1) = W2.recordDate
+AND W1.temperature > W2.temperature;
